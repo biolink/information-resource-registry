6c51f989a34f63fa15ada69106754b048fc0fff3
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2024-03-08 10:49:14 -0800

    get tests to run, get lint to pass

--- tests/test_data.py
+++ tests/test_data.py
@@ -4,7 +4,7 @@ import glob
 import unittest
 
 from linkml_runtime.loaders import yaml_loader
-from information_resource_registry.datamodel.information_resource_registry import InformationResourceContainer
+from src.information_resource_registry.datamodel.information_resource_registry import InformationResourceContainer
 
 ROOT = os.path.join(os.path.dirname(__file__), '..')
 DATA_DIR = os.path.join(ROOT, "src", "data", "examples")
