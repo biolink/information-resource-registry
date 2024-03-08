dfc679baa6f45eef113ca71c7fb7221adb84943f
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2024-03-08 11:34:05 -0800

    regen project

--- src/information_resource_registry/datamodel/information_resource_registry.py
+++ src/information_resource_registry/datamodel/information_resource_registry.py
@@ -1,5 +1,5 @@
 # Auto generated from information_resource_registry.yaml by pythongen.py version: 0.0.1
-# Generation date: 2024-03-08T11:20:09
+# Generation date: 2024-03-08T11:33:03
 # Schema: Information-Resource-Registry-Schema
 #
 # id: https://w3id.org/biolink/information_resource_registry.yaml
