2853e28a50d7edf0214c183385470c84a5f09885
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2024-06-09 10:22:15 -0700

    fix conflicts and fix failing urls

--- project/information_resource_registry.py
+++ project/information_resource_registry.py
@@ -1,5 +1,5 @@
 # Auto generated from information_resource_registry.yaml by pythongen.py version: 0.0.1
-# Generation date: 2024-06-08T18:43:55
+# Generation date: 2024-06-09T10:16:28
 # Schema: Information-Resource-Registry-Schema
 #
 # id: https://w3id.org/biolink/information_resource_registry.yaml
