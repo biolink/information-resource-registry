c6e487bb035fb1b0cda8df7e92bb005729a007cd
GitHub Action <action@github.com>
2024-10-01 23:33:42 +0000

    Regenerate artifacts from information_resource_registry.yaml

--- project/information_resource_registry.py
+++ project/information_resource_registry.py
@@ -1,5 +1,5 @@
 # Auto generated from information_resource_registry.yaml by pythongen.py version: 0.0.1
-# Generation date: 2024-09-27T17:56:03
+# Generation date: 2024-10-01T23:33:32
 # Schema: Information-Resource-Registry-Schema
 #
 # id: https://w3id.org/biolink/information_resource_registry.yaml
