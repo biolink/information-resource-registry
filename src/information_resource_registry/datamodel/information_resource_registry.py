cba6acfd0cd1d1f4b10413bb6a6f02b7969297f5
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2024-10-04 16:48:37 -0700

    fix infores metadata after updates

--- src/information_resource_registry/datamodel/information_resource_registry.py
+++ src/information_resource_registry/datamodel/information_resource_registry.py
@@ -1,5 +1,5 @@
 # Auto generated from information_resource_registry.yaml by pythongen.py version: 0.0.1
-# Generation date: 2024-10-04T12:04:24
+# Generation date: 2024-10-04T16:48:03
 # Schema: Information-Resource-Registry-Schema
 #
 # id: https://w3id.org/biolink/information_resource_registry.yaml
