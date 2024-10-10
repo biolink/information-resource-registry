882e2c1e2aea121f36c9e4b2b8b29230d35fcc05
Max Wang <max@covar.com>
2024-10-10 14:47:05 -0400

    Update Aragorn kp script to not use kp registry

--- src/information_resource_registry/relation_map/aragorn_kps.py
+++ src/information_resource_registry/relation_map/aragorn_kps.py
@@ -1,24 +1,25 @@
-import asyncio
+import requests
 import json
-from kp_registry import Registry
 
 infores_dict = {"infores:aragorn": []}
 
 
-async def main():
-    registry = Registry()
+def main():
+    url = "https://strider.renci.org/kps"
+    response = requests.get(url)
 
-    kps = await registry.retrieve_kps()
+    if response.status_code == 200:
+        try:
+            kps = response.json()
 
-    for kp in kps.values():
-        if kp.get("maturity") == "staging":
-            infores_dict["infores:aragorn"].append(kp.get("infores", "Unknown"))
-    
-    # Sort the list in alphabetical order
-    infores_dict['infores:aragorn'].sort()
+            infores_dict["infores:aragorn"] = kps
 
-    with open('src/information_resource_registry/relation_map/data/aragorn_to_kps.json', 'w') as outfile:
-        json.dump(infores_dict, outfile, indent=2, sort_keys=True)
+            with open('src/information_resource_registry/relation_map/data/aragorn_to_kps.json', 'w') as outfile:
+                json.dump(infores_dict, outfile, indent=2, sort_keys=True)
+        except Exception as e:
+            print(f"Failed to parse list of KPs from Aragorn: {e}")
+    else:
+        print(f"Failed to download list of KPs from Aragorn with status code: {response.status_code}.")
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
