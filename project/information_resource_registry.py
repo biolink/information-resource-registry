4a7a64ee095e258a82897d37deb191bdffca88e2
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2024-10-28 15:23:18 -0700

    regenerate artifacts

--- project/information_resource_registry.py
+++ project/information_resource_registry.py
@@ -1,5 +1,5 @@
 # Auto generated from information_resource_registry.yaml by pythongen.py version: 0.0.1
-# Generation date: 2024-10-28T22:09:38
+# Generation date: 2024-10-28T15:22:54
 # Schema: Information-Resource-Registry-Schema
 #
 # id: https://w3id.org/biolink/information_resource_registry.yaml
