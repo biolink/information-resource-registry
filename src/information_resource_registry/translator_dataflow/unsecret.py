d94aa725c52f46d7b60cbfe165e5adfadb96a8cc
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2024-10-28 15:20:49 -0700

    add unsecret to the automated run, move weekly URL check that inevitably always has at least one broken URL to its own weekly check, allowing sankey generatation to proceed

--- src/information_resource_registry/translator_dataflow/unsecret.py
+++ src/information_resource_registry/translator_dataflow/unsecret.py
@@ -17,7 +17,7 @@ def main():
         print(f"Error downloading file: {response.status_code}")
         exit()
 
-    json.dump(infores_dict, open('data/unsecret.json', 'w'), indent=2, sort_keys=True)
+    json.dump(infores_dict, open('src/information_resource_registry/translator_dataflow/data/unsecret.json', 'w'), indent=2, sort_keys=True)
 
 # Call main() if this script is executed directly
 if __name__ == "__main__":
