2b4a6a3c87fb596123bbe33a71f0e685e0364b04
GitHub Action <action@github.com>
2026-05-25 11:15:03 +0000

    Regenerate artifacts from infores_catalog.yaml

--- src/information_resource_registry/datamodel/information_resource_registry.py
+++ src/information_resource_registry/datamodel/information_resource_registry.py
@@ -1,5 +1,5 @@
 # Auto generated from information_resource_registry.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-05-18T11:10:05
+# Generation date: 2026-05-25T11:14:45
 # Schema: Information-Resource-Registry-Schema
 #
 # id: https://w3id.org/biolink/information_resource_registry.yaml
